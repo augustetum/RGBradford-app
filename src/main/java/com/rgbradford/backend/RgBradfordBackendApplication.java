46c996b5807cf75407c9edafbc71c388f4734339
Augustė Tumaitė <120650538+augustetum@users.noreply.github.com>
2025-06-24 16:41:05 +0200

    Add project dependencies and structure

---
+++ src/main/java/com/rgbradford/backend/RgBradfordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rgbradford.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgBradfordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgBradfordBackendApplication.class, args);
+	}
+
+}
