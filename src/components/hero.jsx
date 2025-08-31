c73fdee20787db2808ff437a7bd2479f05272ecf
Augustas <augustasrink@gmail.com>
2025-09-01 00:59:11 +0300

    mild homepage restyling with breakpoints

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,23 +6,27 @@ function Hero({name}) {
         <>
         <header className='text-center pb-4 text-3xl font-semibold'>
           <h2>Welcome back {name}!</h2>
-          <img src={chemistryImage} alt="" className='drop mx-auto w-auto h-50' />
+          
         </header>
-        <ul className='flex flex-col my-4'>
-          <li className='mr-auto drop'>
-            <a className='' href="protocol.com">
-            <div className='hoverRaise elipsoid  px-4 py-8 font-bold bg-igem-white text-igem-black'>
-              Read the protocol
-            </div>
-            </a>
-          </li>
-          <li className='ml-auto drop'>
-            <a className='' href="tutorial.com">
-            <div className='hoverRaise elipsoid px-4 py-8 font-bold bg-igem-white text-igem-black'>
-              Watch the tutorial
-            </div>
-            </a>
-          </li>
+        
+        <ul className='flex sm:flex-row flex-col items-center justify-center gap-10 my-4'>
+          <img src={chemistryImage} alt="" className='drop h-40 w-40' />
+          <div className="flex sm:flex-col flex-row gap-10 ">
+            <li className=' drop'>
+              <a className='' href="protocol.com">
+              <div className='hoverRaise elipsoid  px-4 py-8 font-bold bg-igem-white text-igem-black'>
+                Read the protocol
+              </div>
+              </a>
+            </li>
+            <li className=' drop'>
+              <a className='' href="tutorial.com">
+              <div className='hoverRaise elipsoid px-4 py-8 font-bold bg-igem-white text-igem-black'>
+                Watch the tutorial
+              </div>
+              </a>
+            </li>
+          </div>
         </ul>
 
         </>
